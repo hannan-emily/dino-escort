650dd85e136140fef5b6ba647ccabb6d7d3751b8
brantchyoga <brantrporter@gmail.com>
2018-03-15 12:06:00 -0700

    Switched over to react router vs state for switching componnets

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 
 
+
 class LandingPage extends Component {
   constructor(props){
     super(props)
@@ -13,6 +14,7 @@ class LandingPage extends Component {
 
 
   render(){
+    if()
     return(
 
     )
