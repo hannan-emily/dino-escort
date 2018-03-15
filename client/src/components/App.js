e673a7fec1ef7aef9755dd8ad6423eff3ca7c48a
brantchyoga <brantrporter@gmail.com>
2018-03-15 13:10:31 -0700

    storing dinos in app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,98 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import {UserProfile} from './UserProfile';
 import axios from 'axios';
 
+var dinos = {
+   dino1:{
+    name: "Irritator Challengeri",
+    category: "land",
+    img_path: "../img/dino-irritator-challengeri.png",
+    img_headshot_path: "",
+    text: "Banjo unicorn butcher pabst venmo jianbing, williamsburg wolf vegan dreamcatcher cornhole master cleanse fixie. Tilde brooklyn taiyaki 8-bit, skateboard cronut post-ironic pok pok. Schlitz post-ironic normcore green juice chartreuse, pop-up polaroid food truck squid sriracha fingerstache la croix salvia.",
+    capacity : "5",
+    cost_hourly: "$50"
+  }
+
+   dino2:{
+    name: "Oxalaia Quilombensis",
+    category: "land",
+    img_path: "../img/dino-oxalaia.png",
+    img_headshot_path: "",
+    text: "Banjo unicorn butcher pabst venmo jianbing, williamsburg wolf vegan dreamcatcher cornhole master cleanse fixie. Tilde brooklyn taiyaki 8-bit, skateboard cronut post-ironic pok pok. Schlitz post-ironic normcore green juice chartreuse, pop-up polaroid food truck squid sriracha fingerstache la croix salvia.",
+    capacity : "15",
+    cost_hourly: "$75"
+  }
+
+   dino3:{
+    name: "Spinosaurus Aegyptiacus",
+    category: "land",
+    img_path: "../img/dino-spinosaurs.png",
+    img_headshot_path: "",
+    text: "Banjo unicorn butcher pabst venmo jianbing, williamsburg wolf vegan dreamcatcher cornhole master cleanse fixie. Tilde brooklyn taiyaki 8-bit, skateboard cronut post-ironic pok pok. Schlitz post-ironic normcore green juice chartreuse, pop-up polaroid food truck squid sriracha fingerstache la croix salvia.",
+    capacity: "30",
+    cost_hourly: "$120"
+  }
+
+   dino4:{
+    name: "Nyctosaurus",
+    category: "air",
+    img_path: "../img/dino-nyctosaurus.png",
+    img_headshot_path: "",
+    text: "Banjo unicorn butcher pabst venmo jianbing, williamsburg wolf vegan dreamcatcher cornhole master cleanse fixie. Tilde brooklyn taiyaki 8-bit, skateboard cronut post-ironic pok pok. Schlitz post-ironic normcore green juice chartreuse, pop-up polaroid food truck squid sriracha fingerstache la croix salvia.",
+    capacity : "1",
+    cost_hourly: "$20"
+  }
+
+   dino5:{
+    name: "Pterodaustro",
+    category: "air",
+    img_path: "../img/dino-pterodaustro.png",
+    img_headshot_path: "",
+    text: "Banjo unicorn butcher pabst venmo jianbing, williamsburg wolf vegan dreamcatcher cornhole master cleanse fixie. Tilde brooklyn taiyaki 8-bit, skateboard cronut post-ironic pok pok. Schlitz post-ironic normcore green juice chartreuse, pop-up polaroid food truck squid sriracha fingerstache la croix salvia.",
+    capacity : "2",
+    cost_hourly: "$40"
+  }
+
+   dino6:{
+    name: "Pteranodon",
+    category: "air",
+    img_path: "../img/dino-pteranodon.png",
+    img_headshot_path: "",
+    text: "Banjo unicorn butcher pabst venmo jianbing, williamsburg wolf vegan dreamcatcher cornhole master cleanse fixie. Tilde brooklyn taiyaki 8-bit, skateboard cronut post-ironic pok pok. Schlitz post-ironic normcore green juice chartreuse, pop-up polaroid food truck squid sriracha fingerstache la croix salvia.",
+    capacity: "2",
+    cost_hourly: "$40"
+  }
+
+   dino7:{
+    name: "Shastasaurus Pacificus",
+    category: "water",
+    img_path: "../img/dino-shastasaurus.png",
+    img_headshot_path: "",
+    text: "Banjo unicorn butcher pabst venmo jianbing, williamsburg wolf vegan dreamcatcher cornhole master cleanse fixie. Tilde brooklyn taiyaki 8-bit, skateboard cronut post-ironic pok pok. Schlitz post-ironic normcore green juice chartreuse, pop-up polaroid food truck squid sriracha fingerstache la croix salvia.",
+    capacity: "100",
+    cost_hourly: "$300"
+  }
+
+   dino8:{
+    name: "Opthalmusarus Iceniucs",
+    category: "water",
+    img_path: "../img/dino-opthalmusaurus.png",
+    img_headshot_path: "",
+    text: "Banjo unicorn butcher pabst venmo jianbing, williamsburg wolf vegan dreamcatcher cornhole master cleanse fixie. Tilde brooklyn taiyaki 8-bit, skateboard cronut post-ironic pok pok. Schlitz post-ironic normcore green juice chartreuse, pop-up polaroid food truck squid sriracha fingerstache la croix salvia.",
+    capacity: "20",
+    cost_hourly: "$90"
+  }
+
+   dino9:{
+    name: "Mosasaurs Hoffmannii",
+    category: "water",
+    img_path: "../img/dino-mosasurus.png",
+    img_headshot_path: "",
+    text: "Banjo unicorn butcher pabst venmo jianbing, williamsburg wolf vegan dreamcatcher cornhole master cleanse fixie. Tilde brooklyn taiyaki 8-bit, skateboard cronut post-ironic pok pok. Schlitz post-ironic normcore green juice chartreuse, pop-up polaroid food truck squid sriracha fingerstache la croix salvia.",
+    capacity: "7",
+    cost_hourly: "$60"
+  }
+}
+
 class App extends Component {
   constructor(props){
     super(props)
@@ -64,10 +156,16 @@ class App extends Component {
               <nav className="nav">
                 <Link className='linkstyle' to='/'>Landing Page</Link>
                 <Link className='linkstyle' to="/cart">Cart</Link>
+                <Link to="/detail"/>
                 <UserProfile user={theUser} logout={this.logout} />
               </nav>
               <Route exact path='/' component={LandingPage} />
               <Route path='/cart' component={Cart} />
+              <Route path='/detail' component={()=> (
+                <Details
+                  dino={dinos}
+                />)}
+              />
             </div>
           </Router>
         </div>
