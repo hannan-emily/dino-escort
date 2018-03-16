2ae0b2a72fd4eca857ead17e979752af0abc2e2a
brantchyoga <brantrporter@gmail.com>
2018-03-15 17:48:26 -0700

    add styling and functionality to cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -5,9 +5,27 @@ import Login from './Login';
 export const Cart = props => {
   var dinosInCart = props.cart
   console.log(dinosInCart);
-  return(
-    <div>
-      <h1>This is Cart</h1>
+  var jsxDinos = dinosInCart.map(dino => {
+    <div className='dinocard'>
+      <h3 className='dinotitle'>{dino.name}</h3>
+      <div className='dinoimage'>
+        <img className='image' src='{dino.img_path}'/>
+      </div>
     </div>
-  )
+  })
+
+  if(!dinosInCart){
+    return(
+      <div className='container'>
+        <h1>Cart in empty</h1>
+      </div>
+    )
+  } else {
+    return(
+      <div className='container'>
+        <h1>You Have Selected</h1>
+        {jsxDinos}
+      </div>
+    )
+  }
 }
